821b3631e73de968641fcc1648142cf0121c0f67
fti11 <mxonstruck@gmail.com>
2023-08-16 14:38:48 +0900

    선언 업데이트

---
+++ GameBase/Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class GameData 
+{
+    public int UserLevel = 1;
+    public string UserName = "Player";
+
+    public int Gold = 0;
+    public int Dia = 0;
+}
